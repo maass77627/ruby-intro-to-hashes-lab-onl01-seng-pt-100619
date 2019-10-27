ad19d28ef2b019ca8499e7963070fa72ee529ef0
kelly r maass <kmaass77627@gmail.com>
2019-10-27 00:20:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,27 +21,7 @@ monopoly = {railroads: {:pieces => 4, names: {reading_railroad: {}, pennsylvania
       :two_pieces_owned => 50,
       :three_pieces_owned => 100,
       :four_pieces_owned => 200
-    }
+    }}}
 end
 
 def monopoly_with_fourth_tier
-monopoly[:railroads] = {}
-	monopoly[:railroads][:pieces] = 4
-	monopoly[:railroads][:names] = {}
-	monopoly[:railroads][:rent_in_dollars] = {}
-
-	monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
-	monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
-	monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
-	monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
-	monopoly[:railroads][:names][:reading_railroad] = {}
-	monopoly[:railroads][:names][:pennsylvania_railroad] = {}
-	monopoly[:railroads][:names][:b_and_o_railroad] = {}
-	monopoly[:railroads][:names][:shortline] = {}
-	
-	monopoly[:railroads][:names][:reading_railroad][:mortgage_value] = "$100"
-	monopoly[:railroads][:names][:pennsylvania_railroad][:mortgage_value] = "$200" 
-	monopoly[:railroads][:names][:b_and_o_railroad][:mortgage_value] = "$400"
-	monopoly[:railroads][:names][:shortline][:mortgage_value] = "$800"
-	return monopoly
-end
