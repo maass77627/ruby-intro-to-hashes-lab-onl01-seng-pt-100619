881382ed1dc4dfd5fbe8405e5bee242b9b1127ac
kelly r maass <kmaass77627@gmail.com>
2019-10-27 00:09:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,6 +10,14 @@ def monopoly
 	monopoly = {railroads: {}} 
 	end 
 	
+	def monopoly_with_second_tier
+	monopoly = {[:railroads] = {}}
+	monopoly[:railroads][:pieces] = 4
+	monopoly[:railroads][:names] = {}
+	monopoly[:railroads][:rent_in_dollars] = {}
+	return monopoly
+	end 
+
 	
 
 def monopoly_with_third_tier
