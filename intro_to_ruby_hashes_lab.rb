04b102c049b1560e6e409e45bf3bcc5aa1285068
kelly r maass <kmaass77627@gmail.com>
2019-10-27 00:14:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,11 +11,7 @@ def monopoly
 	end 
 	
 	def monopoly_with_second_tier
-	monopoly = {[:railroads] = {}}
-	monopoly[:railroads][:pieces] = 4
-	monopoly[:railroads][:names] = {}
-	monopoly[:railroads][:rent_in_dollars] = {}
-	return monopoly
+	monopoly = {railroads: {:pieces => 4, names: {}, rent_in_dollars: {}}}
 	end 
 
 	
